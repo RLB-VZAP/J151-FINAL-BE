6b65a14dc4c20718e111d900ac27505f8280e68d
Magdeli-Potgieter <magdelipotgieter.mp@gmail.com>
2026-06-25 08:54:06 +0200

    Created the UserDAO (interface).

---
+++ src/main/java/com/vzap/trytons/dao/UserDAO.java
@@ -0,0 +1,14 @@
+package com.vzap.trytons.dao;
+
+import com.vzap.trytons.model.User;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserDAO {
+    Optional<User> getUserById(UUID userId);
+    Optional<User> getUserByEmail(String email);
+    Optional<User> getUserByUsername(String username);
+    Optional<User> registerUser(User user);
+    Optional<User> updateUser(User user);
+}
