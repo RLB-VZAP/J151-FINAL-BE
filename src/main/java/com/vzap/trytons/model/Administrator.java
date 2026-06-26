be76da3cfef10d6362624ff1ce31390f8b87cd03
Magdeli-Potgieter <magdelipotgieter.mp@gmail.com>
2026-06-26 08:56:15 +0200

    Created and completed the AdministratorDAOImpl class. Changed the method signature of deactivateAccount to deactivateUserAccount. Replaced @Builder with @SuperBuilder in the models User, RegisteredUser, and Administrator.

--- src/main/java/com/vzap/trytons/model/Administrator.java
+++ src/main/java/com/vzap/trytons/model/Administrator.java
@@ -1,12 +1,16 @@
 package com.vzap.trytons.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.experimental.SuperBuilder;
 
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
 public class Administrator extends User {
 
     private int adminLevel;
