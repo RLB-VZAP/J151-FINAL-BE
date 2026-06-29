1c6ff53b3465b030863948012c9272c9bc04fa50
Jaunte Kelvin Garcia <kelvinjauntegarcia@gmail.com>
2026-06-29 11:21:05 +0200

    Created basic ClubResource with lombok api annotations

--- src/main/java/com/vzap/trytons/resource/ClubResource.java
+++ src/main/java/com/vzap/trytons/resource/ClubResource.java
@@ -1,6 +1,5 @@
 package com.vzap.trytons.resource;
 
-import jakarta.inject.Inject;
 import jakarta.ws.rs.ApplicationPath;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.Path;
@@ -10,10 +9,10 @@ import jakarta.ws.rs.core.MediaType;
 import java.util.logging.Logger;
 
 @ApplicationPath("/api")
-@Path("/player")
+@Path("/club")
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
-public class PlayerResource {
-    private static final Logger LOGGER = Logger.getLogger(UserResources.class.getName());
+public class ClubResource {
+    private static final Logger LOGGER = Logger.getLogger(ClubResource.class.getName());
 
 }
