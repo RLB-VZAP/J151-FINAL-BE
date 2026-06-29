df3e0719b2bfa6a978c93a3983d7711c6396bc1d
Jarryd Lautenbach <jarryd.lautenbach3@gmail.com>
2026-06-29 02:17:18 +0200

    Fixed registration error handling by implementing a mapper Exception

---
+++ src/main/java/com/vzap/trytons/mapper/JAXExceptionMapper.java
@@ -0,0 +1,28 @@
+package com.vzap.trytons.mapper;
+
+import com.vzap.trytons.dto.ErrorResponse;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.ext.ExceptionMapper;
+import jakarta.ws.rs.ext.Provider;
+
+import java.util.stream.Collectors;
+@Provider
+public class JAXExceptionMapper implements ExceptionMapper<ConstraintViolationException> {
+    @Override
+    public Response toResponse(ConstraintViolationException e) {
+        String message = e.getConstraintViolations()
+                .stream()
+                .map(ConstraintViolation::getMessage)
+                .distinct()
+                .collect(Collectors.joining(", ")); //CSV Format for build
+    if (message.isBlank()) {
+        message = "Invalid request structure";
+    }
+    ErrorResponse error = ErrorResponse.of(message, "VALIDATION_ERROR");
+    return Response.status(Response.Status.BAD_REQUEST)
+            .entity(error)
+            .build();
+    }
+}
