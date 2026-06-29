ddae38de9c8f8368c94d556b6da253099b580a13
JustPhobia <jarryd.lautenbach3@gmail.com>
2026-06-29 15:29:24 +0200

    introduce own and opponent fantasy team view DTOs and update FantasyTeamService access methods

--- src/main/java/com/vzap/trytons/service/FantasyTeamService.java
+++ src/main/java/com/vzap/trytons/service/FantasyTeamService.java
@@ -1,5 +1,15 @@
 package com.vzap.trytons.service;
 
-public interface FantasyTeamService {
+import com.vzap.trytons.dto.ViewOpponentTeamDTO;
+import com.vzap.trytons.dto.ViewOwnTeamDTO;
+import com.vzap.trytons.model.FantasyTeam;
 
+import java.util.UUID;
+
+public interface FantasyTeamService {
+    //TODO Import FantasyTeamDTO when available
+    FantasyTeam createTeam(UUID registeredUserId,FantasyTeamDTO fantasyTeamDTO);
+    ViewOpponentTeamDTO viewOpponentTeam(UUID teamId);
+    ViewOwnTeamDTO viewOwnTeam(UUID registeredUserId, UUID teamId);
+    FantasyTeam updateTeam(UUID registeredId,  UUID teamId, FantasyTeamDTO fantasyTeamDTO);
 }
