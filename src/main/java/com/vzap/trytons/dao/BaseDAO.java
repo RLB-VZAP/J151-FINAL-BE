d4ce4d9a12fc74dfb71e5c378ba3005c8332bd38
JustPhobia <jarryd.lautenbach3@gmail.com>
2026-06-30 15:15:29 +0200

    Removed unnecessary resource closer

--- src/main/java/com/vzap/trytons/dao/BaseDAO.java
+++ src/main/java/com/vzap/trytons/dao/BaseDAO.java
@@ -11,30 +11,4 @@ public abstract class BaseDAO {
     protected Connection getConnection() throws SQLException {
         return DBConnectionManager.getConnection();
     }
-
-    protected void closeResources(Connection con, Statement stmt, ResultSet rs) {
-        if (rs != null) {
-            try {
-                rs.close();
-            } catch (SQLException e) {
-                e.printStackTrace(); //Error should actually be logged.
-            }
-        }
-
-        if (stmt != null) {
-            try {
-                stmt.close();
-            } catch (SQLException e) {
-                e.printStackTrace(); //Error should actually be logged.
-            }
-        }
-
-        if (con != null) {
-            try {
-                con.close();
-            } catch (SQLException e) {
-                e.printStackTrace(); //Error should actually be logged.
-            }
-        }
-    }
 }
