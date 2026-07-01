e37ae10a251b6159f847a7ef391c70cb20074c6e
Lindsay988 <lindsay.kirstens16@gmail.com>
2026-07-01 20:26:41 +0200

    Removed: -The variables not needed in the LeagueRequestDTO and LeagueResponseDTO. -Removed the methods that used the DTO's in the LeagueDAO. -Commented out code you might need, and deleted some methods that were wrong. Added: -Logger to the LeagueDAOImpl -Added some comments to the LeagueDaoImpl.deactivateLeague method. -Added the empty methods.

--- src/main/java/com/vzap/trytons/dao/LeagueDAO.java
+++ src/main/java/com/vzap/trytons/dao/LeagueDAO.java
@@ -1,32 +1,17 @@
 package com.vzap.trytons.dao;
-
-import com.vzap.trytons.dto.CreateLeagueRequest;
-import com.vzap.trytons.dto.LeagueCodeResponse;
-import com.vzap.trytons.dto.LeagueResponse;
 import com.vzap.trytons.model.League;
-
-import java.sql.SQLException;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface LeagueDAO {
-
-    UUID createLeague(CreateLeagueRequest request, UUID managerId, String leagueCode) throws SQLException;
-
-    Optional<League> findById(UUID leagueId) throws SQLException;
-
-    List<League> findAllActive() throws SQLException;
-
-    Optional<LeagueResponse> findResponseById(UUID leagueId) throws SQLException;
-
-    List<LeagueResponse> findResponsesByManager(UUID managerId) throws SQLException;
-
-    boolean existsByLeagueCode(String leagueCode) throws SQLException;
-
-    Optional<UUID> findLeagueIdByCode(String leagueCode) throws SQLException;
-
-    Optional<LeagueCodeResponse> findLeagueCode(UUID leagueId) throws SQLException;
-
-    boolean deactivateLeague(UUID leagueId) throws SQLException;
+   League saveLeague(League league);
+    Optional<League> findLeagueById(UUID leagueId);
+    List<League> findAllLeagues();
+    List<League> findLeaguesByLeagueManager(UUID userId);//could also be with their name
+    Optional<League> findLeagueByLeagueCode(UUID leagueCode);
+    boolean existsByLeagueCode(String leagueCode);
+    boolean deactivateLeague(UUID leagueId);
+    League updateLeague(League league);
+    boolean deleteLeague(UUID leagueId);
 }
