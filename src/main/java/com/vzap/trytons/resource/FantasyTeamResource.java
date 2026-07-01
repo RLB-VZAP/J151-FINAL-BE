c8b09f77bb37724dc612c4073ab42c00129fd8d8
Jaunte Kelvin Garcia <kelvinjauntegarcia@gmail.com>
2026-07-01 09:03:55 +0200

    In FantasyTeamResource built supporting response methods and "serverError" and "unexpected"

--- src/main/java/com/vzap/trytons/resource/FantasyTeamResource.java
+++ src/main/java/com/vzap/trytons/resource/FantasyTeamResource.java
@@ -1,11 +1,22 @@
 package com.vzap.trytons.resource;
 
-import jakarta.ws.rs.ApplicationPath;
-import jakarta.ws.rs.Consumes;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
+import com.vzap.trytons.dto.ErrorResponse;
+import com.vzap.trytons.dto.FantasyTeamPlayerSelectionRequestDTO;
+import com.vzap.trytons.dto.FantasyTeamRequestDTO;
+import com.vzap.trytons.dto.FantasyTeamResponseDTO;
+import com.vzap.trytons.exceptions.DataAccessException;
+import com.vzap.trytons.service.FantasyTeamService;
+import jakarta.inject.Inject;
+import jakarta.validation.Valid;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.Context;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.UriInfo;
 
+import java.net.URI;
+import java.util.UUID;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 @ApplicationPath("/api")
@@ -14,5 +25,16 @@ import java.util.logging.Logger;
 @Produces(MediaType.APPLICATION_JSON)
 public class FantasyTeamResource {
     private static final Logger LOGGER = Logger.getLogger(UserResources.class.getName());
+    @Inject
+    private FantasyTeamService fantasyTeamService;
 
+    private Response serverError(String message , DataAccessException e) {
+      LOGGER.log(Level.SEVERE, message, e);
+      return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(e.getMessage()).build();
+    }
+
+    private Response unexpected(Exception e) {
+        LOGGER.log(Level.SEVERE, "Unexpected error in PositionResource.", e);
+        return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(ErrorResponse.of("An unexpected error occurred.", "INTERNAL_SERVER_ERROR")).build();
+    }
 }
