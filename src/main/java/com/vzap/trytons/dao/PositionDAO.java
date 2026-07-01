65638434b1ef5cf3bddf8058129f3ef29b2876eb
Lindsay988 <lindsay.kirstens16@gmail.com>
2026-07-01 09:03:28 +0200

    Changed the return type of the FindAllPosition method from Optional to List in the PositionDAO. Removed the DeletePosition method from the PositionDAO.

--- src/main/java/com/vzap/trytons/dao/PositionDAO.java
+++ src/main/java/com/vzap/trytons/dao/PositionDAO.java
@@ -8,9 +8,8 @@ public interface PositionDAO {
     Optional<Position> findById(UUID PositionId);
     Optional<Position> findByName(String positionName);
     Optional<Position> findByPositionCategory(String positionCategory);
-    Optional<Position> findAllPositions();
+    List<Position> findAllPositions();
     public boolean createPosition(Position position);
     public boolean updatePosition(Position position);
-    public boolean deletePosition(Position position);
     public boolean existsByName(String positionName);
 }
