57a82307a848686769cfd8a3466ed8cd28ab2209
Jaunte Kelvin Garcia <kelvinjauntegarcia@gmail.com>
2026-07-01 09:14:21 +0200

    In FantasyTeamResource built the end point to allow the creation of FantasyTeams

--- src/main/java/com/vzap/trytons/resource/FantasyTeamResource.java
+++ src/main/java/com/vzap/trytons/resource/FantasyTeamResource.java
@@ -4,6 +4,7 @@ import com.vzap.trytons.dto.ErrorResponse;
 import com.vzap.trytons.dto.FantasyTeamPlayerSelectionRequestDTO;
 import com.vzap.trytons.dto.FantasyTeamRequestDTO;
 import com.vzap.trytons.dto.FantasyTeamResponseDTO;
+import com.vzap.trytons.exceptions.ConflictException;
 import com.vzap.trytons.exceptions.DataAccessException;
 import com.vzap.trytons.service.FantasyTeamService;
 import jakarta.inject.Inject;
@@ -27,6 +28,23 @@ public class FantasyTeamResource {
     private static final Logger LOGGER = Logger.getLogger(UserResources.class.getName());
     @Inject
     private FantasyTeamService fantasyTeamService;
+
+    @POST
+    public Response createFantasyTeam(@QueryParam("userId") UUID userId,@Valid FantasyTeamRequestDTO request , @Context UriInfo uriInfo ){
+        try{
+            FantasyTeamResponseDTO created = fantasyTeamService.createTeam(userId, request);
+            URI location = uriInfo.getAbsolutePathBuilder().path(created.getTeamId().toString()).build();
+            return Response.created(location).entity(created).build();
+        }catch(ConflictException e){
+            return Response.status(Response.Status.CONFLICT).entity(ErrorResponse.of(e.getMessage() , e.getErrorCode())).build();v
+        }catch (DataAccessException e){
+            return serverError("Failed to create fantasy-team",e);
+        }catch (Exception e){
+            return unexpected(e);
+        }
+    }
+
+
 
     private Response serverError(String message , DataAccessException e) {
       LOGGER.log(Level.SEVERE, message, e);
