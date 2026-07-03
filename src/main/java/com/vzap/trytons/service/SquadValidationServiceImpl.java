4c24f018720829d56853320554056072c1cd6d77
JustPhobia <jarryd.lautenbach3@gmail.com>
2026-07-03 10:36:47 +0200

    Removed generic type parameter in SquadValidationService

--- src/main/java/com/vzap/trytons/service/SquadValidationServiceImpl.java
+++ src/main/java/com/vzap/trytons/service/SquadValidationServiceImpl.java
@@ -2,6 +2,7 @@ package com.vzap.trytons.service;
 
 import com.vzap.trytons.dao.PlayerDAO;
 import com.vzap.trytons.dto.SquadValidationResultDTO;
+import com.vzap.trytons.exceptions.ValidationException;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 
@@ -15,11 +16,16 @@ public class SquadValidationServiceImpl implements SquadValidationService {
     PlayerDAO playerDAO;
 
     @Override
-    public <SquadValidationResult> SquadValidationResult validateSquad(List<UUID> proposedPlayerIds, BigDecimal maximumSquadValue) {
+    public SquadValidationResultDTO validateSquad(List<UUID> proposedPlayerIds, BigDecimal maximumSquadValue) {
+
         return null;
     }
 
-    private void validateSquadSize() {
+    private void validateSquadSize(List<UUID> proposedPlayerIds) {
+        int size = proposedPlayerIds.size();
+        if (size == 20){
+            throw new ValidationException("Squad size is not valid. Squad size must be 20.");
+        }
     }
 
     private void validateDuplicatePlayers() {
