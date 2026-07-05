11720ae3afb5fc77b572c77a8b9757c8a0894bcf
Jarryd Lautenbach <jarryd.lautenbach3@gmail.com>
2026-07-05 22:59:10 +0200

    Partial completion of SquadValidationService and SquadValidationServiceImpl

--- src/main/java/com/vzap/trytons/service/SquadValidationServiceImpl.java
+++ src/main/java/com/vzap/trytons/service/SquadValidationServiceImpl.java
@@ -2,21 +2,30 @@ package com.vzap.trytons.service;
 
 import com.vzap.trytons.dao.PlayerDAO;
 import com.vzap.trytons.dto.SquadValidationResultDTO;
-import com.vzap.trytons.exceptions.ValidationException;
+import com.vzap.trytons.model.Player;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 
-import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
-
+/*
+Squad size
+Duplicate players
+Null or nonexistent player IDs
+Player active status
+Player availability
+Minimum position requirements
+Maximum position requirements
+ */
 @ApplicationScoped
 public class SquadValidationServiceImpl implements SquadValidationService {
     @Inject
     PlayerDAO playerDAO;
 
     @Override
-    public SquadValidationResultDTO validateSquad(List<UUID> proposedPlayerIds, BigDecimal maximumSquadValue) {
+    public SquadValidationResultDTO validateSquad(List<UUID> proposedPlayerIds) {
         SquadValidationResultDTO results;
 
         return null;
@@ -25,17 +34,42 @@ public class SquadValidationServiceImpl implements SquadValidationService {
     private void validateSquadSize(List<UUID> proposedPlayerIds, SquadValidationResultDTO results) {
         int size = proposedPlayerIds.size();
         if (size != 20){
-            throw new ValidationException("Squad size is not valid. Squad size must be 20.");
+            results.addError("INVALID_SQUAD_SIZE", "Squad size must be 20", "List<UUID> proposedPlayerIds");
         }
     }
 
-    private void validateDuplicatePlayers() {
+    private void validateDuplicatePlayers(List<UUID> proposedPlayerIds,SquadValidationResultDTO results) {
+        int found = 0;
+        for (UUID playerId : proposedPlayerIds) {
+            for (UUID playerId2 : proposedPlayerIds) {
+                if (playerId.equals(playerId2)) {
+                    found++;
+                }
+            }
+        }
+        if (found >1){
+            results.addError("DUPLICATE_PLAYERS", "Duplicate player found", "List<UUID> proposedPlayerIds");
+        }
     }
 
-    private void validatePlayerIdsExist() {
+    private void validatePlayerIdsExist(List<UUID> proposedPlayerIds,SquadValidationResultDTO results) {
+        try {
+
+            for (UUID playerId : proposedPlayerIds) {
+                playerDAO.getPlayerById(playerId).orElseThrow(() -> new RuntimeException("PLAYER_NOT_FOUND"));
+            }
+        }catch (RuntimeException e){
+            results.addError(e.getMessage(), "Player not found", "List<UUID> proposedPlayerIds");
+        }
     }
 
-    private void validatePlayerAvailability() {
+    private void validatePlayerAvailability(List<UUID> proposedPlayerIds,SquadValidationResultDTO results) {
+        for (UUID playerId : proposedPlayerIds) {
+            Player present = playerDAO.getPlayerById(playerId).orElseThrow(() -> new RuntimeException("PLAYER_NOT_FOUND"));
+            if (present) {
+
+            }
+        }
     }
 
     private void validatePositionRules() {
