1ab8919376947b3277970b60b2983360510bd5fc
Timothy-Madlala <21mttimothymadlala@gmail.com>
2026-07-06 12:01:30 +0200

    Stubs for this ticket complete. CompetitionProcessingResource, CompetitionProcessingService, CompetitionProcessingSummaryDTO.

---
+++ src/main/java/com/vzap/trytons/resource/CompetitionProcessingResource.java
@@ -0,0 +1,10 @@
+package com.vzap.trytons.resource;
+
+import com.vzap.trytons.service.CompetitionProcessingService;
+import jakarta.inject.Inject;
+
+public class CompetitionProcessingResource {
+
+    @Inject
+    private CompetitionProcessingService competitionProcessingService;
+}
