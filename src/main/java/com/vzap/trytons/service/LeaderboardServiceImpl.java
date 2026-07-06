9c64f9b0c81440460ecb0d4f90b47c915f952520
Jaunte Kelvin Garcia <kelvinjauntegarcia@gmail.com>
2026-07-06 10:10:33 +0200

    Stubs for leader board , Dao, Service layer , DTO's , API

---
+++ src/main/java/com/vzap/trytons/service/LeaderboardServiceImpl.java
@@ -0,0 +1,17 @@
+package com.vzap.trytons.service;
+
+import com.vzap.trytons.dto.LeaderboardRefreshResultDTO;
+
+import java.util.UUID;
+
+public class LeaderboardServiceImpl implements LeaderboardService {
+    @Override
+    public LeaderboardRefreshResultDTO refreshLeagueLeaderboard(UUID actorUserId, UUID leagueId) {
+        return null;
+    }
+
+    @Override
+    public LeaderboardRefreshResultDTO refreshOverallLeaderboard(UUID actorUserId) {
+        return null;
+    }
+}
