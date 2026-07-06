33a1a3e35957de89856befa1a0d6ccd2e99c8163
Lindsay988 <lindsay.kirstens16@gmail.com>
2026-07-06 11:02:44 +0200

    Created ScoringRule backend stub pack: DTO, DAO, DAOImpl, Service, ServiceImpl and Resource.

---
+++ src/main/java/com/vzap/trytons/resource/ScoringRuleResource.java
@@ -0,0 +1,10 @@
+package com.vzap.trytons.resource;
+
+import com.vzap.trytons.service.ScoringRuleService;
+import jakarta.inject.Inject;
+
+public class ScoringRuleResource {
+    //STUB
+    @Inject
+    private ScoringRuleService scoringRuleService;
+}
