eef3037a38ad5f3d65cd8fb00abc01961f466c25
Sameer-Sacoor <sameersacoorv3@gmail.com>
2026-07-07 09:26:44 +0200

    saving my changes to update project

--- src/main/java/com/vzap/trytons/service/LeagueServiceImpl.java
+++ src/main/java/com/vzap/trytons/service/LeagueServiceImpl.java
@@ -5,7 +5,11 @@ import com.vzap.trytons.dao.LeagueDAO;
 import com.vzap.trytons.dao.LeagueMembershipDAO;
 import com.vzap.trytons.dto.LeagueRequestDTO;
 import com.vzap.trytons.dto.LeagueResponseDTO;
+import com.vzap.trytons.enums.LeagueMemberRole;
+import com.vzap.trytons.enums.LeagueType;
+import com.vzap.trytons.model.League;
 
+import java.util.List;
 import java.util.UUID;
 
 public class LeagueServiceImpl implements LeagueService{
@@ -21,12 +25,58 @@ public class LeagueServiceImpl implements LeagueService{
     }
 
     @Override
-    public LeagueResponseDTO createLeague(LeagueResponseDTO createLeague, LeagueRequestDTO request, UUID currentUserId) {
+    public LeagueResponseDTO createLeague(LeagueRequestDTO request, UUID currentUserId){
 
-        if (request == null){
-            request.getLeagueName();
+        if (request == null) {
+            throw new IllegalArgumentException("League request cannot be null.");
         }
-        return null;
+
+        if (request.getLeagueName() == null ||
+                request.getLeagueName().isBlank()) {
+
+            throw new IllegalArgumentException("League name is required.");
+        }
+
+        if (leagueDAO.findLeagueByName(request.getLeagueName()).isPresent()) {
+            throw new IllegalArgumentException("League name already exists.");
+        }
+
+        League league = new League();
+
+        league.setLeagueName(request.getLeagueName());
+        league.setLeagueType(request.getLeagueType());
+
+        League savedLeague = leagueDAO.createLeague(league);
+
+        LeagueMemberRole role;
+
+        if (request.getLeagueType() == LeagueType.PRIVATE) {
+            role = LeagueMemberRole.MANAGER;
+        } else {
+            role = LeagueMemberRole.MEMBER;
+        }
+
+        membershipDAO.createMembership( savedLeague.getLeagueId(), currentUserId, role);
+
+//        List<FantasyTeam> teams = fantasyTeamDAO.findTeamsByOwner(currentUserId);
+//
+//        if (teams.isEmpty()) {
+//            throw new IllegalArgumentException(
+//                    "You must create a fantasy team before creating a league.");
+//        }
+//
+//        League league = new League();
+//
+//        league.setLeagueName(request.getLeagueName());
+//        league.setLeagueType(request.getLeagueType());
+//
+//        if (request.getLeagueType() == LeagueType.PRIVATE) {
+//            league.setLeagueManagerId(currentUserId);
+//        }
+//
+//        League savedLeague = leagueDAO.createLeague(league);
+
+        return new LeagueResponseDTO(savedLeague);
     }
 
     @Override
@@ -42,5 +92,10 @@ public class LeagueServiceImpl implements LeagueService{
     @Override
     public boolean isLeagueMember(UUID leagueId, UUID userId) {
         return false;
+    }
+
+    @Override
+    public List<LeagueResponseDTO> getAllLeagues(UUID currentUserId) {
+        return List.of();
     }
 }
