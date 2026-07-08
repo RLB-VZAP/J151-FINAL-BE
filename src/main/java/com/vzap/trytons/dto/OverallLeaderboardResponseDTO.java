57b348f96c28dce4797ff8dd355c1d2658a141e2
Lindsay988 <lindsay.kirstens16@gmail.com>
2026-07-08 23:39:42 +0200

    Completed the OverallLeaderboardResponseDTO.

--- src/main/java/com/vzap/trytons/dto/OverallLeaderboardResponseDTO.java
+++ src/main/java/com/vzap/trytons/dto/OverallLeaderboardResponseDTO.java
@@ -4,10 +4,14 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 
 public class OverallLeaderboardResponseDTO {
+    private String season;
+    private List<LeaderboardEntryResponseDTO> standings;
 
 }
