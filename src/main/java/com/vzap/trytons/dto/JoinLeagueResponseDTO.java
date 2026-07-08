639c5fc10fd16f8363669d957f30efb6a81a5e61
Lindsay988 <lindsay.kirstens16@gmail.com>
2026-07-08 12:53:06 +0200

    Added lomabrk builder.

--- src/main/java/com/vzap/trytons/dto/JoinLeagueResponseDTO.java
+++ src/main/java/com/vzap/trytons/dto/JoinLeagueResponseDTO.java
@@ -1,9 +1,6 @@
 package com.vzap.trytons.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.UUID;
 
@@ -12,6 +9,7 @@ import java.util.UUID;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class JoinLeagueResponseDTO {
     private UUID leagueId;
     private String leagueName;
