e952b2c5b27acefb534ee4368c6b7e428f83af51
Jarryd Lautenbach <jarryd.lautenbach3@gmail.com>
2026-07-08 23:42:11 +0200

    removed imports

--- src/main/java/com/vzap/trytons/dao/FixtureDAOImpl.java
+++ src/main/java/com/vzap/trytons/dao/FixtureDAOImpl.java
@@ -2,9 +2,6 @@ package com.vzap.trytons.dao;
 
 import com.vzap.trytons.enums.FixtureStatus;
 import com.vzap.trytons.model.Fixture;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
--- src/main/java/com/vzap/trytons/model/Fixture.java
+++ src/main/java/com/vzap/trytons/model/Fixture.java
@@ -8,8 +8,6 @@ import lombok.Setter;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.UUID;
 
 @NoArgsConstructor
