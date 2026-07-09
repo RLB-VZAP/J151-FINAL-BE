88152eb2f34d26b617617b358603b6e966c175dc
Jarryd Lautenbach <jarryd.lautenbach3@gmail.com>
2026-07-09 03:08:01 +0200

    feat(points): define fantasy point breakdown service stubs

--- src/main/java/com/vzap/trytons/service/FantasyPointBreakdownService.java
+++ src/main/java/com/vzap/trytons/service/FantasyPointBreakdownService.java
@@ -6,8 +6,6 @@ import java.util.List;
 import java.util.UUID;
 
 public interface FantasyPointBreakdownService {
-
     FantasyPointBreakdownResponseDTO getBreakdownById(UUID breakdownId);
-
     List<FantasyPointBreakdownResponseDTO> listBreakdownsForPoints(UUID pointsId);
 }
