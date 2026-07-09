46c3c017a28914300b304cd8277a0edef4930ca4
ChrisbeanCosley <christan1234@gmail.com>
2026-07-09 10:40:28 +0200

    added findById, findByStatId, findFinalByStatId methods to FantasyPointsDAOImpl

--- src/main/java/com/vzap/trytons/dao/FantasyPointsDAOImpl.java
+++ src/main/java/com/vzap/trytons/dao/FantasyPointsDAOImpl.java
@@ -3,6 +3,8 @@ package com.vzap.trytons.dao;
 import com.vzap.trytons.exceptions.DataAccessException;
 import com.vzap.trytons.model.*;
 import java.sql.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -42,17 +44,105 @@ public class FantasyPointsDAOImpl extends BaseDAO implements FantasyPointsDAO {
 
     @Override
     public Optional<FantasyPoints> findById(UUID pointsId) {
-        throw new UnsupportedOperationException("FantasyPointsDAOImpl stub: findById is not implemented yet.");
+        String query =  "SELECT * FROM fantasyPoints WHERE pointsId = ?";
+
+        try(Connection con = getConnection();
+            PreparedStatement ps = con.prepareStatement(query)){
+            ps.setString(1, pointsId.toString());
+
+            try(ResultSet rs = ps.executeQuery()){
+                if(rs.next()){
+
+                    FantasyPoints fp = FantasyPoints.builder()
+                            .pointsId(pointsId)
+                            .statId(UUID.fromString(rs.getString("statId")))
+                            .totalPoints(rs.getInt("totalPoints"))
+                            .calculationVersion(rs.getInt("calculationVersion"))
+                            .finalVersion(rs.getBoolean("isFinal"))
+                            .calculationDate(rs.getObject("calculatedAt", LocalDateTime.class))
+                            .build();
+
+                    return Optional.of(fp);
+
+                }
+            }
+        } catch (SQLException e) {
+            LOG.log(Level.SEVERE, "Unable to find fantasy points with ID", e);
+            throw new DataAccessException("Unable to find fantasy points with ID", e);
+        }
+        return Optional.empty();
+
     }
 
     @Override
     public List<FantasyPoints> findByStatId(UUID statId) {
-        throw new UnsupportedOperationException("FantasyPointsDAOImpl stub: findByStatId is not implemented yet.");
+
+        String query = "SELECT * FROM fantasyPoints WHERE statId = ?";
+        List<FantasyPoints> fantasyPoints = new ArrayList<>();
+
+        try (Connection con = getConnection();
+             PreparedStatement ps = con.prepareStatement(query))   {
+
+            ps.setString(1, statId.toString());
+
+            try(ResultSet rs = ps.executeQuery()){
+                while (rs.next()){
+
+                    FantasyPoints fp = FantasyPoints.builder()
+                            .pointsId(UUID.fromString(rs.getString("pointsId")))
+                            .statId(statId)
+                            .totalPoints(rs.getInt("totalPoints"))
+                            .calculationVersion(rs.getInt("calculationVersion"))
+                            .finalVersion(rs.getBoolean("isFinal"))
+                            .calculationDate(rs.getObject("calculatedAt", LocalDateTime.class))
+                            .build();
+
+                    fantasyPoints.add(fp);
+
+                }
+            }
+
+        } catch (SQLException e) {
+            LOG.log(Level.SEVERE, "Unable to find fantasy points by statId", e);
+            throw new DataAccessException("Unable to find fantasy points by statId", e);
+        }
+
+        return fantasyPoints;
     }
 
     @Override
     public Optional<FantasyPoints> findFinalByStatId(UUID statId) {
-        throw new UnsupportedOperationException("FantasyPointsDAOImpl stub: findFinalByStatId is not implemented yet.");
+
+        String query = "SELECT * FROM fantasyPoints WHERE isFinal = ? AND statId = ?";
+
+        try (Connection con = getConnection();
+            PreparedStatement ps = con.prepareStatement(query)) {
+
+            ps.setBoolean(1, true);
+            ps.setString(2, statId.toString());
+
+            try (ResultSet rs = ps.executeQuery()){
+                if (rs.next()){
+                    FantasyPoints fp = FantasyPoints.builder()
+                            .pointsId(UUID.fromString(rs.getString("pointsId")))
+                            .statId(statId)
+                            .totalPoints(rs.getInt("totalPoints"))
+                            .calculationVersion(rs.getInt("calculationVersion"))
+                            .finalVersion(rs.getBoolean("isFinal"))
+                            .calculationDate(rs.getObject("calculatedAt", LocalDateTime.class))
+                            .build();
+
+                    return Optional.of(fp);
+
+                }
+            }
+
+        } catch (SQLException e) {
+            LOG.log(Level.SEVERE, "Unable to find fantasy points by statId", e);
+            throw new DataAccessException("Unable to find fantasy points by statId", e);
+        }
+
+        return Optional.empty();
     }
 
     @Override
