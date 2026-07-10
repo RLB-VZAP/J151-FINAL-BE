6ca1e3cb5b81dbc771a9897833f295188319e7d3
Jarryd Lautenbach <jarryd.lautenbach3@gmail.com>
2026-07-10 12:09:21 +0200

    Added Builder lombok annotation

--- src/main/java/com/vzap/trytons/dto/FantasyTeamPlayerSelectionRequestDTO.java
+++ src/main/java/com/vzap/trytons/dto/FantasyTeamPlayerSelectionRequestDTO.java
@@ -1,12 +1,14 @@
 package com.vzap.trytons.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
 import java.util.UUID;
 
 @Getter
 @AllArgsConstructor
+@Builder
 public class FantasyTeamPlayerSelectionRequestDTO {
     private UUID playerId;
 }
