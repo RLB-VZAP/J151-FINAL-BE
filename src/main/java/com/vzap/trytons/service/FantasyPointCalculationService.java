1fcfef8fd90084f00d663fa3e7c4a4697db859e2
ChrisbeanCosley <christan1234@gmail.com>
2026-07-13 15:00:48 +0200

    removed unnecessary imports in FantasyPointCalculationService and FantasyPointCalculationServiceImpl

--- src/main/java/com/vzap/trytons/service/FantasyPointCalculationService.java
+++ src/main/java/com/vzap/trytons/service/FantasyPointCalculationService.java
@@ -2,8 +2,6 @@ package com.vzap.trytons.service;
 
 import com.vzap.trytons.dto.FantasyPointCalculationResultDTO;
 
-import java.util.UUID;
-
 public interface FantasyPointCalculationService {
     FantasyPointCalculationResultDTO calculateForFixture(String fixtureId);
 }
