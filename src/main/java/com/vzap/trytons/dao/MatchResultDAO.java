cbaf82c6c0f847920bebe50acb299df737b6e5ce
ChrisbeanCosley <christan1234@gmail.com>
2026-07-13 11:29:29 +0200

    added methods specified in ticket to MatchResultDAO interface

--- src/main/java/com/vzap/trytons/dao/MatchResultDAO.java
+++ src/main/java/com/vzap/trytons/dao/MatchResultDAO.java
@@ -2,11 +2,16 @@ package com.vzap.trytons.dao;
 
 import com.vzap.trytons.model.MatchResult;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface MatchResultDAO {
 
-    Optional<MatchResult> findByFixtureId(UUID fixtureId);
-    Optional<MatchResult> save(MatchResult matchResult);
+    MatchResult save(MatchResult matchResult);
+    Optional<MatchResult> findById(UUID resultId);
+    Optional<MatchResult> findCurrentByFixtureId(UUID fixtureId);
+    List<MatchResult> findAllByFixtureId(UUID fixtureId);
+    int getNextSimulationRunNumber(UUID fixtureId);
+    int markAllFixtureResultsNotCurrent(UUID fixtureId);
 }
