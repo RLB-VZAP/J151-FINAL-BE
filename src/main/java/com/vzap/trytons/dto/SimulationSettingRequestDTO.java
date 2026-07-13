745cdb0b22b65113fb3080a8b742700f5d7bffaa
Jarryd Lautenbach <jarryd.lautenbach3@gmail.com>
2026-07-13 13:56:20 +0200

    fix(simulation): restore simulation setting response DTO

--- src/main/java/com/vzap/trytons/dto/SimulationSettingRequestDTO.java
+++ src/main/java/com/vzap/trytons/dto/SimulationSettingRequestDTO.java
@@ -10,6 +10,8 @@ import java.math.BigDecimal;
 @NoArgsConstructor
 @AllArgsConstructor
 public class SimulationSettingRequestDTO {
+    private String season;
+    private BigDecimal playerAbilityWeight;
     private BigDecimal playerFormWeight;
     private BigDecimal teamBalanceWeight;
     private BigDecimal randomVariationWeight;
