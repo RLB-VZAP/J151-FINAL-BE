1fab2d8731bdedb5d7090ddbbc4debfb08565fb1
Jarryd Lautenbach <jarryd.lautenbach3@gmail.com>
2026-07-14 14:21:26 +0200

    MatchSimulationServiceImpl method start and blocked by FantasyTeam issues

--- src/main/java/com/vzap/trytons/service/MatchSimulationServiceImpl.java
+++ src/main/java/com/vzap/trytons/service/MatchSimulationServiceImpl.java
@@ -1,14 +1,38 @@
 package com.vzap.trytons.service;
 
+import com.vzap.trytons.dao.FixtureDAO;
+import com.vzap.trytons.dao.RegisteredUserDAO;
+import com.vzap.trytons.dao.UserDAO;
 import com.vzap.trytons.dto.MatchResultResponseDTO;
+import com.vzap.trytons.exceptions.AuthorisationException;
+import com.vzap.trytons.exceptions.ResourceNotFoundException;
+import com.vzap.trytons.model.FantasyTeam;
+import com.vzap.trytons.model.Fixture;
+import com.vzap.trytons.model.RegisteredUser;
+import com.vzap.trytons.model.User;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.BadRequestException;
 
 import java.util.UUID;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @ApplicationScoped
 public class MatchSimulationServiceImpl implements MatchSimulationService {
+    private static final Logger LOG = Logger.getLogger(MatchProcessingServiceImpl.class.getName());
+
+    @Inject
+    private RegisteredUserDAO registeredUserDAO;
+    @Inject
+    private FixtureDAO fixtureDAO;
+
     @Override
     public MatchResultResponseDTO simulateFixture(UUID actorUserId, UUID fixtureId) {
-        throw new UnsupportedOperationException("MatchSimulationServiceImpl.simulateFixture is a stub.");
+            User user = registeredUserDAO.getRegisteredUserById(actorUserId).orElseThrow(()-> new AuthorisationException("User not found"));
+            Fixture fixture = fixtureDAO.findById(fixtureId).orElseThrow(()-> new ResourceNotFoundException("Fixture not found"));
+            FantasyTeam teamA =  fixture.getTeamA();
+            FantasyTeam teamB =  fixture.getTeamB();
+
     }
 }
