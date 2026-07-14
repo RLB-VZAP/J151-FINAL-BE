e9434196708af53527cfa5eaffa82ab913c9a660
ChrisbeanCosley <christan1234@gmail.com>
2026-07-14 13:31:02 +0200

    added fields to TeamScoreUpdateResultDTO

--- src/main/java/com/vzap/trytons/dto/TeamScoreUpdateResultDTO.java
+++ src/main/java/com/vzap/trytons/dto/TeamScoreUpdateResultDTO.java
@@ -1,11 +1,21 @@
 package com.vzap.trytons.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
+@Builder
 
 public class TeamScoreUpdateResultDTO {
+
+    private String fixtureId;
+    private String teamId;
+    private int teamATotal;
+    private int teamBTotal;
+    private String outcome;
 
 }
