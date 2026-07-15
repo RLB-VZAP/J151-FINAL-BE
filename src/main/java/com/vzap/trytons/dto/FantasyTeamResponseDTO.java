1cbc1f0c6a0799762255be96a04784a57eb4f838
Lindsay988 <lindsay.kirstens16@gmail.com>
2026-07-16 01:17:23 +0200

    Fixed.

--- src/main/java/com/vzap/trytons/dto/FantasyTeamResponseDTO.java
+++ src/main/java/com/vzap/trytons/dto/FantasyTeamResponseDTO.java
@@ -21,6 +21,6 @@ public class FantasyTeamResponseDTO {
     private Integer weeklyPoints;
     private Integer totalPoints;
     private Boolean valid;
-    private Boolean locked;
     private List<FantasyTeamPlayerSelectionResponseDTO> selectedPlayers;
+
 }
