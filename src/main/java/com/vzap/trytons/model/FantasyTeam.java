9c9d0b3d025214bf32d1306a4702af1e4af0e9ef
Lindsay988 <lindsay.kirstens16@gmail.com>
2026-07-15 18:16:09 +0200

    Took out two variables to match the schema exactly, added AllAgrsConstructor and fixed a variable name to snake case.

--- src/main/java/com/vzap/trytons/model/FantasyTeam.java
+++ src/main/java/com/vzap/trytons/model/FantasyTeam.java
@@ -1,25 +1,24 @@
 package com.vzap.trytons.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
-
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
 @NoArgsConstructor
+@AllArgsConstructor
 @Getter
 @Setter
 @SuperBuilder
 public class FantasyTeam {
     private UUID teamId;
     private String teamName;
-    private BigDecimal totalTeamValue;
     private BigDecimal remainingBudget;
     private LocalDateTime creationDate;
-    private int totalPoints;
     private Boolean isValid;
-    private UUID ownerUserId;
+    private UUID owner_user_id;
 }
