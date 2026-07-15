3be3f1c3217ca90d9a1b51274bb20192beffe3c7
ChrisbeanCosley <christan1234@gmail.com>
2026-07-15 21:37:04 +0200

    fixed methods in MatchResultServiceImpl and fixed MatchResultResponseDTO

--- src/main/java/com/vzap/trytons/dto/MatchResultResponseDTO.java
+++ src/main/java/com/vzap/trytons/dto/MatchResultResponseDTO.java
@@ -1,5 +1,6 @@
 package com.vzap.trytons.dto;
 
+import com.vzap.trytons.enums.MatchTeamSide;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,10 +14,12 @@ import java.util.UUID;
 public class MatchResultResponseDTO {
     private UUID resultId;
     private UUID fixtureId;
+    private UUID teamAId;
+    private UUID teamBId;
     private int simulationRunNumber;
     private int teamAScore;
     private int teamBScore;
-    private String winnerSide;
+    private MatchTeamSide winnerSide;
     private boolean isDraw;
     private boolean approved;
     private boolean isCurrent;
