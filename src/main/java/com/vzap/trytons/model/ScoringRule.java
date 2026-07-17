71fcaf6d923801967fecda2b83e7d9a141500eb6
ChrisbeanCosley <christan1234@gmail.com>
2026-07-17 16:30:05 +0200

    added @AllArgsConstructor and @Builder to ScoringRule model

--- src/main/java/com/vzap/trytons/model/ScoringRule.java
+++ src/main/java/com/vzap/trytons/model/ScoringRule.java
@@ -1,14 +1,14 @@
 package com.vzap.trytons.model;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.UUID;
 
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class ScoringRule {
 
     private UUID ruleId;
