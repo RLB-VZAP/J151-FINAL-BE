6f54d6de830850c0059e09ccaf2c1b255412fca3
Lindsay988 <lindsay.kirstens16@gmail.com>
2026-07-20 14:12:42 +0200

    Added for reports

--- src/main/java/com/vzap/trytons/dao/admin/LogDAO.java
+++ src/main/java/com/vzap/trytons/dao/admin/LogDAO.java
@@ -1,11 +1,12 @@
 package com.vzap.trytons.dao.admin;
 
 
+import com.vzap.trytons.model.admin.Log;
 import com.vzap.trytons.model.admin.LogActionCount;
 
 import java.util.List;
 
-public interface Log  {
-    List<com.vzap.trytons.model.admin.Log> findRecentLogs(int limit);
+public interface LogDAO {
+    List<Log> findRecentLogs(int limit);
     List<LogActionCount> countByActionType();
 }
