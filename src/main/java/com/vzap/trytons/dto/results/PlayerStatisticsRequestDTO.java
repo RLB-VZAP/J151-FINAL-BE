354021b09bb4b244370b8a91830ac45c2497dd79
Jarryd Lautenbach <jarryd.lautenbach3@gmail.com>
2026-07-21 01:30:04 +0200

    Updated Lombok location

--- src/main/java/com/vzap/trytons/dto/results/PlayerStatisticsRequestDTO.java
+++ src/main/java/com/vzap/trytons/dto/results/PlayerStatisticsRequestDTO.java
@@ -1,14 +1,14 @@
 package com.vzap.trytons.dto.results;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.UUID;
 
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class PlayerStatisticsRequestDTO {
     private UUID resultId;
     private UUID teamId;
