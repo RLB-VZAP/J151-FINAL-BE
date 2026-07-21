8aff64c6a38cec201e589c24d117fe8be07dfe34
Jarryd Lautenbach <jarryd.lautenbach3@gmail.com>
2026-07-21 08:59:34 +0200

    Add user profile view/update and change-password backend flow
    Adds GET/PUT /api/users/profile and POST /api/users/profile/change-password.
    RegisteredUserServicesImpl gains getProfile/updateProfile/changePassword,
    backed by new UserDAO methods updateProfileDetails and updatePasswordHash
    (parameterised SQL). ProfileResponseDTO never exposes passwordHash.
    
    Classes:
    - dto/auth/ProfileResponseDTO.java
    - dto/auth/ProfileUpdateRequestDTO.java
    - dto/auth/ChangePasswordRequestDTO.java
    - resource/auth/ProfileResource.java
    - service/auth/RegisteredUserServices.java
    - service/auth/RegisteredUserServicesImpl.java
    - dao/auth/UserDAO.java
    - dao/auth/UserDAOImpl.java

---
+++ src/main/java/com/vzap/trytons/dto/auth/ChangePasswordRequestDTO.java
@@ -0,0 +1,21 @@
+package com.vzap.trytons.dto.auth;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ChangePasswordRequestDTO {
+    @NotBlank(message = "Current password can not be blank")
+    private String currentPassword;
+
+    @NotBlank(message = "New password cannot be blank")
+    @Size(min = 8, message = "New password needs to be at least 8 characters long")
+    private String newPassword;
+}
